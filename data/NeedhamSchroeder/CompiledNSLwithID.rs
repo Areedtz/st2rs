43d9428197b168cd6a319f22ad101c5d805c43c7
Elitsa <elit0451@edu.eal.dk>
2022-04-19 20:13:43 +0200

    Raw NeedhamSchroederLowe implementations in Rust
    Co-authored-by: Hundter <Hundter@users.noreply.github.com>
    Co-authored-by: Areedtz <Areedtz@users.noreply.github.com>

---
+++ data/NeedhamSchroeder/CompiledNSLwithID.rs
@@ -0,0 +1,187 @@
+#![allow(warnings)]
+
+extern crate session_types;
+use session_types::*;
+use std::{marker};
+use serde::{Serialize, Deserialize};
+use std::thread;
+use std::marker::PhantomData;
+use serde::de::DeserializeOwned;
+
+#[derive(Serialize, Deserialize)]
+pub struct Repr<T>(Vec<u8>, PhantomData<T>);
+
+impl<T : Serialize + DeserializeOwned> Represent<T> for Repr<T> {
+    fn from_repr(b: Repr<T>) -> T { bincode::deserialize(&b.0[..]).unwrap() }
+    fn to_repr(b: T) -> Repr<T> { Repr(bincode::serialize(&b).unwrap(), PhantomData) }
+}
+
+trait Represent<T> {
+    fn from_repr(_: Repr<T>) -> T;
+    fn to_repr(_: T) -> Repr<T>;
+}
+
+fn send<E, P, A: marker::Send + Serialize + DeserializeOwned + 'static>(c: Chan<E, Send<Repr<A>, P>>, v: A) -> Chan<E, P> { c.send(Repr::to_repr(v)) }
+fn recv<E, P, A: marker::Send + Serialize + DeserializeOwned + 'static>(c: Chan<E, Recv<Repr<A>, P>>) -> (Chan<E, P>, A) { let (c, x) = c.recv(); (c, Repr::from_repr(x)) }
+fn close<E>(c: Chan<E, Eps>) { c.close() }
+
+type AB = Send<Repr<bytes>, Recv<Repr<bytes>, Send<Repr<bytes>, Eps>>>;
+type APublic = Send<Repr<pkey>, Send<Repr<id>, Recv<Repr<id>, Eps>>>;
+
+type BA = Recv<Repr<bytes>, Send<Repr<bytes>, Recv<Repr<bytes>, Eps>>>;
+type BPublic = Send<Repr<pkey>, Eps>;
+
+type PublicB = Recv<Repr<pkey>, Eps>;
+type PublicA = Recv<Repr<pkey>, Recv<Repr<id>, Send<Repr<id>, Eps>>>;
+
+type id = /* unimplemented */;
+type bytes = /* unimplemented */;
+type skey = /* unimplemented */;
+type pkey = /* unimplemented */;
+
+fn pk(a1: skey) -> pkey {
+	unimplemented!();
+}
+fn sk2id(a1: skey) -> id {
+	unimplemented!();
+}
+fn id2pk(a1: id) -> pkey {
+	unimplemented!();
+}
+fn aenc(a1: pkey, a2: bytes) -> bytes {
+	unimplemented!();
+}
+fn adec(a1: skey, a2: bytes) -> bytes {
+	unimplemented!();
+}
+fn nawrap(a1: id, a2: bytes) -> bytes {
+	unimplemented!();
+}
+fn naunwrap(a1: bytes) -> (id, bytes) {
+	unimplemented!();
+}
+fn nanbwrap(a1: id, a2: bytes, a3: bytes) -> bytes {
+	unimplemented!();
+}
+fn nanbunwrap(a1: bytes) -> (id, bytes, bytes) {
+	unimplemented!();
+}
+fn fresh_id() -> id {
+	unimplemented!();
+}
+fn fresh_bytes() -> bytes {
+	unimplemented!();
+}
+fn fresh_skey() -> skey {
+	unimplemented!();
+}
+fn fresh_pkey() -> pkey {
+	unimplemented!();
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+	#[test]
+	fn test_equation_0() {
+		let k = fresh_skey();
+		assert_eq!(id2pk(sk2id(k)), pk(k));
+	}
+	#[test]
+	fn test_equation_1() {
+		let k = fresh_skey();
+		let m = fresh_bytes();
+		assert_eq!(adec(k, aenc(pk(k), m)), m);
+	}
+	#[test]
+	fn test_equation_2() {
+		let idp = fresh_id();
+		let nonce = fresh_bytes();
+		assert_eq!(naunwrap(nawrap(idp, nonce)), (idp, nonce));
+	}
+	#[test]
+	fn test_equation_3() {
+		let idp = fresh_id();
+		let nonce1 = fresh_bytes();
+		let nonce2 = fresh_bytes();
+		assert_eq!(nanbunwrap(nanbwrap(idp, nonce1, nonce2)), (idp, nonce1, nonce2));
+	}
+}
+
+fn a(c_APublic: Chan<(), APublic>, c_AB: Chan<(), AB>, idb: id, ska: skey) {
+	let c_APublic = send(c_APublic, pk(ska));
+	let c_APublic = send(c_APublic, sk2id(ska));
+	let (c_APublic, idx) = recv(c_APublic);
+	let pkx = id2pk(idx);
+	let na = fresh_bytes();
+	let ida = sk2id(ska);
+	println!("A started");
+	let ct = aenc(pkx, nawrap(ida, na));
+	let c_AB = send(c_AB, ct);
+	let (c_AB, ctb) = recv(c_AB);
+	let (v0, v1, nx) = nanbunwrap(adec(ska, ctb));
+	if idx == v0 && na == v1 {
+		let enc_nb = aenc(pkx, nx);
+		let c_AB = send(c_AB, enc_nb);
+		let pkb = id2pk(idb);
+		if pkx == pkb {
+			println!("A ended");
+			close(c_AB);
+			close(c_APublic);
+		} else {
+			println!("A ended (not talking to B)");
+			close(c_AB);
+			close(c_APublic);
+		};
+	};
+}
+
+fn b(c_BPublic: Chan<(), BPublic>, c_BA: Chan<(), BA>, ida: id, skb: skey) {
+	let c_BPublic = send(c_BPublic, pk(skb));
+	let (c_BA, cta) = recv(c_BA);
+	let (v2, ny) = naunwrap(adec(skb, cta));
+	if ida == v2 {
+		let nb = fresh_bytes();
+		let idb = sk2id(skb);
+		println!("B started");
+		let pka = id2pk(ida);
+		let ct = aenc(pka, nanbwrap(idb, ny, nb));
+		let c_BA = send(c_BA, ct);
+		let (c_BA, z) = recv(c_BA);
+		let z_nb = adec(skb, z);
+		if z_nb == nb {
+			println!("B ended");
+			close(c_BA);
+			close(c_BPublic);
+		} else {
+			println!("B ended (not talking to A)");
+			close(c_BA);
+			close(c_BPublic);
+		};
+	};
+}
+
+fn public(c_PublicA: Chan<(), PublicA>, c_PublicB: Chan<(), PublicB>, idb: id) {
+	let (c_PublicA, pka) = recv(c_PublicA);
+	let (c_PublicA, ida) = recv(c_PublicA);
+	let (c_PublicB, pkb) = recv(c_PublicB);
+	let c_PublicA = send(c_PublicA, idb);
+	close(c_PublicB);
+	close(c_PublicA);
+}
+
+fn main() {
+	let ska = fresh_skey();
+	let skb = fresh_skey();
+	let idb = sk2id(skb);
+	let ida = sk2id(ska);
+
+	let (c_AB, c_BA) = session_channel();
+	let (c_BPublic, c_PublicB) = session_channel();
+	let (c_APublic, c_PublicA) = session_channel();
+
+	let a_t = thread::spawn(move || a(c_APublic, c_AB, idb, ska));
+	let b_t = thread::spawn(move || b(c_BPublic, c_BA, ida, skb));
+	let public_t = thread::spawn(move || public(c_PublicA, c_PublicB, idb));
+	let _ = (a_t.join(), b_t.join(), public_t.join());
+}
